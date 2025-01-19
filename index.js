1bba003a2e358dfe3b7c11f4094d02cfc0c5a2e5
Kyran <kyransolomon5@gmail.com>
2025-01-19 20:10:01 +0800

    Initial Commit!

---
+++ index.js
@@ -0,0 +1,48 @@
+const game = {
+    playerOne: 0,
+    playerTwo: 0,
+    currentPlayer: 'Player 1' //indicates which player is going to move
+}
+let cols = 3
+let rows = 3
+let board = [] //board of the game
+const buttons = document.querySelectorAll('.btn'); //selects all my     button
+
+
+//how to set up the board 3*3
+// for(let i = 0; i < cols;i++){
+//     let row = []
+//     board.push(row)
+//     for(let j = 0; j < rows;j++){
+//         row.push('')
+//     }
+// }       
+
+//how the game works(conditions = moves per player)
+buttons.forEach((button, index) => {
+    button.addEventListener('click' , () => {
+        if(game.currentPlayer == 'Player 1'){
+            board[index] = 'X'
+            button.textContent = 'X'
+            game.currentPlayer = 'Player 2'
+        }
+        else{
+            board[index] = 'O'
+            button.textContent = 'O'
+            game.currentPlayer = 'Player 1'
+        }
+    button.disabled = true;
+    console.log(game.currentPlayer)
+    console.log(board)
+    console.log(board[index])
+    })
+
+})
+// current problem: its a nodeList and the content that I want to put inside the board is not going in why??    
+
+
+
+//winning condition
+
+
+//[[]]
